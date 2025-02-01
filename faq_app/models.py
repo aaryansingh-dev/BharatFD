beeb134bcb34521c3790bb1435aa4203917622bb
Aaryan Singh <aaryan.jsingh@gmail.com>
2025-01-31 21:51:27 -0700

    Update FAQ's save method and create serializers.py for FAQ model

--- faq_app/models.py
+++ faq_app/models.py
@@ -9,4 +9,18 @@ class FAQ(models.Model):
     question = models.TextField()
     answer = RichTextField()
 
+    def save(self, *args, **kwargs):
+        for lang in LANGUAGES:
+            attr_question= f'question_{lang}'
+            attr_answer = f'answer_{lang}'
+
+            if not getattr(self, attr_question):
+                setattr(self, attr_question, translate(self.question, lang))
+            if not getattr(self, attr_answer):
+                setattr(self, attr_answer, translate(self.answer, lang))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.question
+
 
